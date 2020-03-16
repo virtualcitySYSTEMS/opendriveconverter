2ed389fccba57716721f19099cddce9fb11fa05c
Maximilian Sindram <msindram@virtualcitysystems.de>
2020-03-16 10:03:58 +0100

    bug fix translation

--- src/main/java/de/vcs/utils/geometry/Transformation.java
+++ src/main/java/de/vcs/utils/geometry/Transformation.java
@@ -13,7 +13,7 @@ public class Transformation {
     public static Geometry transform(Geometry geom, double hdg, double xOffset, double yOffset) {
         AffineTransformation trans = new AffineTransformation();
         trans.rotate(hdg);
-        trans.translate(-xOffset, -yOffset);
+        trans.translate(xOffset, yOffset);
         return trans.transform(geom);
     }
 
@@ -21,7 +21,7 @@ public class Transformation {
         ArrayList<Geometry> transformedGeometries = new ArrayList<>();
         AffineTransformation trans = new AffineTransformation();
         trans.rotate(hdg);
-        trans.translate(-xOffset, -yOffset);
+        trans.translate(xOffset, yOffset);
         geoms.forEach(g -> transformedGeometries.add(trans.transform(g)));
         return transformedGeometries;
     }
