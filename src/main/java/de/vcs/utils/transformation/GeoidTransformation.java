b0607cdc90f3cc4cf569b863f9a4acdee25fbf97
Maximilian Sindram <master@geotrend.de>
2021-01-20 10:28:36 +0100

    work on geoid

--- src/main/java/de/vcs/utils/transformation/GeoidTransformation.java
+++ src/main/java/de/vcs/utils/transformation/GeoidTransformation.java
@@ -3,6 +3,8 @@ package de.vcs.utils.transformation;
 import org.geotools.geometry.DirectPosition3D;
 import org.geotools.referencing.operation.DefaultMathTransformFactory;
 import org.geotools.referencing.operation.transform.EarthGravitationalModel;
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.GeometryFactory;
 import org.opengis.referencing.FactoryException;
 import org.opengis.referencing.operation.MathTransform;
 import org.opengis.referencing.operation.TransformException;
@@ -16,6 +18,17 @@ public final class GeoidTransformation {
         DirectPosition3D dest = new DirectPosition3D();
         mt.transform(new DirectPosition3D(lon, lat, height), dest);
         return dest.z;
+    }
+
+    public static void transformWGSGeoid(Geometry g) throws TransformException, FactoryException {
+        if (g.getSRID() == 4326) {
+            EarthGravitationalModel.Provider provider = new EarthGravitationalModel.Provider();
+            DefaultMathTransformFactory factory = new DefaultMathTransformFactory();
+            MathTransform mt = factory.createParameterizedTransform(provider.getParameters().createValue());
+            DirectPosition3D dest = new DirectPosition3D();
+            g.getCoordinates();
+            mt.transform(new DirectPosition3D(lon, lat, height), dest);
+        }
     }
 }
 
